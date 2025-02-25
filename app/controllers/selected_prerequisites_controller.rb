261c533ab486630c1be3fc207095e306a2823aa9
Jason Rocha <jasonrr19@gmail.com>
2025-02-24 23:16:51 -0800

    sprereq functional

--- app/controllers/selected_prerequisites_controller.rb
+++ app/controllers/selected_prerequisites_controller.rb
@@ -1,7 +1,24 @@
 class SelectedPrerequisitesController < ApplicationController
+  before_action :set_selected_prerequisite, only: %i[edit update]
+
   def edit
   end
 
   def update
+    if @selected_prerequisite.update(selected_prerequisite_params)
+      redirect_to tender_path(@selected_prerequisite.tender), notice: "updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def selected_prerequisite
+    @selected_prerequisite = SelectedPrerequisite.find(params[:id])
+  end
+
+  def selected_prerequisite_params
+    params.require(:selected_prerequisite).permit(:description, :prerequisite)
   end
 end
