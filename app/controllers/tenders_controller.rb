ca5fc2641ae3f46ffef84f70db37a071bbd827a0
rafaela yazawa <yazawrafa@gmail.com>
2025-02-25 17:12:12 +0900

    Tenders can be created

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -3,11 +3,32 @@ class TendersController < ApplicationController
   end
 
   def show
+    raise
   end
 
   def new
+    @prerequisites = Prerequisite.all
+    @tender = Tender.new(params[:tender_params])
+    @tender.selected_prerequisites.build # needed for nested form
+  end
+
+  def create
+    @tender = Tender.new(tender_params)
+    @tender.user = current_user
+    if @tender.save
+      redirect_to tender_path(@tender)
+    else
+      raise
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def update
+  end
+
+  private
+
+  def tender_params
+    params.require(:tender).permit(:title, :synopsis, selected_prerequisites_attributes: [:prerequisite_id])
   end
 end
--- app/models/selected_prerequisite.rb
+++ app/models/selected_prerequisite.rb
@@ -1,7 +1,7 @@
 class SelectedPrerequisite < ApplicationRecord
   belongs_to :tender
   belongs_to :prerequisite
-  validates :description, presence: true, length: { minimum: 10 }
+  # validates :description, presence: true, length: { minimum: 10 }
   validates :prerequisite, uniqueness: { scope: :tender }
   # enum status: { approved: 0, rejected: 1 } #dont want but need boolean?
   #should require at least one prerequisite to be selectedf
