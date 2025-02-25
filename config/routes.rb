8d16bd127c19bb4dc8be28cb30659279767303d4
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-02-25 11:24:59 +0900

    routes controllers added

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,24 @@
 Rails.application.routes.draw do
-  get 'compatible_responses/edit'
-  get 'compatible_responses/update'
-  get 'selected_prerequisites/edit'
-  get 'selected_prerequisites/update'
-  get 'selected_prerequisite/edit'
-  get 'selected_prerequisite/update'
-  get 'submissions/index'
-  get 'submissions/show'
-  get 'submissions/update'
-  get 'submissions/create'
-  get 'tenders/index'
-  get 'tenders/show'
-  get 'tenders/new'
-  get 'tenders/update'
+
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :tenders, only: [:index, :show, :new, :edit, :update] do
+    resources :submissions, only: [:index, :create]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :submissions, only: [:show, :update]
+
+  resources :compatible_responses, only: [:edit, :update]
+
+  resources :selected_prerequisites, only: [:edit, :update]
+
+  namespace :owner do
+    resources :tenders, only: [:index]
+  end
+
+  namespace :bidder do
+    resources :submissions, only: [:index]
+  end
+
 end
