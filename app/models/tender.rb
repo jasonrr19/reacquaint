0727a252c0ceb33786a07496fa101097f934d959
rafaela yazawa <yazawrafa@gmail.com>
2025-03-05 13:01:22 +0900

    we now can upload pdfs

--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,12 +1,13 @@
 class Tender < ApplicationRecord
   belongs_to :user
+
   has_many :selected_prerequisites, dependent: :destroy
   accepts_nested_attributes_for :selected_prerequisites, allow_destroy: true
   has_many :submissions, dependent: :destroy
   has_many :users, through: :submissions
   validates :synopsis, presence: true
   validates :title, presence: true
-  has_many_attached :documents
+  has_one_attached :document
   # validates :published, presence: true
 
   include PgSearch::Model
