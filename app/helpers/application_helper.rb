57c3faadba98320c47fab1e6436b59aca04c44dd
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2025-03-07 12:42:39 +0900

    landing page updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def score_color(score)
+    if score == "-"
+      return "text-gray"
+    elsif score > 80
+      return "text-success"
+    elsif score > 50
+      return "text-info"
+    else
+      return "text-danger"
+    end
+  end
 end
